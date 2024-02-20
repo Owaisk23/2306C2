3a70f3aa7e585e05547fd4e3d6162f1f226a2f87
Owais Ahmed Khan <owais1566@gmail.com>
2024-02-20 12:41:13 +0500

    added mysql intro queries

---
+++ MySQL/introclass.sql
@@ -0,0 +1,33 @@
+-- CRUD in MYSQL
+-- create database command 
+create database 2306C2;
+
+CREATE DataBase hosp;
+-- DELETE dB command
+Drop DATABASE HOSP;
+-- use db command 
+use 2306C2;
+
+create table students(
+	stdId INT,
+    stdName varchar(25),
+    stdAge int
+);
+
+create table teachers(
+	stdId INT,
+    stdName varchar(25),
+    stdAge int
+);
+
+
+
+
+
+
+
+
+
+
+
+
