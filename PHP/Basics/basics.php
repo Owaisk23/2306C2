3f1e5e8792beedbfd12b78673f6122aefb7664a1
Owais Ahmed Khan <owais1566@gmail.com>
2024-03-16 11:58:15 +0500

    added php basics

---
+++ PHP/Basics/basics.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    // BASICS
+    // Variable declare $varName
+    // $num = -23;
+
+    // $name = "Owais";
+    // $Name = "Owais Ahmed";
+
+    // print($name);
+    // print($Name);
+    // print($num);
+
+    // echo "<h1>This is my full name $Name</h1>";
+
+    // concatenation dot(.)
+
+    // echo "<h1>Sum of two numbers are ".($num + 23)."</h1>";
+
+    // number methods: abs, floor, ceil, round, sqrt
+    // echo abs($num); //23
+    // echo ceil(9.2); //10
+    // echo floor(11.7); //11
+    // echo round(9.5); //10
+    // echo round(9.4); //9
+    // echo sqrt(625); //25
+
+    //strng methods: ucwords, strrev
+    // $str = "owais ahmed khan";
+
+    // echo $str;
+    // echo ucwords($str);
+    // echo strrev($str);
+    // echo str_word_count($str);
+
+    // Datatypes in PHP
+    // Number
+    $num1 = 45; //int
+    $num2 = 32.5; //float
+    $num3 = 1.76534783228472312; //double
+
+    // string
+
+    $str1 = "Php class in 2306C2 batch"; //string
+
+    // boolean
+
+    $userResponse = true;
+    $isValid = false;
+
+
+?>
+</body>
+</html>
---
+++ PHP/Basics/first.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Owais Ahmed Khan";
+?>
