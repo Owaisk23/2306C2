4998f75af6eff981a99e02053c29951c8f65bb78
Owais Ahmed Khan <owais1566@gmail.com>
2024-03-28 11:57:57 +0500

    added types of arrays in php

--- PHP/Basics/arrays.php
+++ PHP/Basics/arrays.php
@@ -37,16 +37,16 @@
         // }
     
         // Multidimensional Arrays
-        $result = [
-            ["Owais" ,78],
-            // 00      01
-            ["Haris" ,88],
-            // 10     11
-            ["Ebad" ,98],
-            // 20    21
-            ["Afzal" ,68]
-            // 30     31
-        ];
+        // $result = [
+        //     ["Owais" ,78 ],
+        //     // 00      01
+        //     ["Haris" ,88],
+        //     // 10     11
+        //     ["Ebad" ,98],
+        //     // 20    21
+        //     ["Afzal" ,68]
+        //     // 30     31
+        // ];
 
         // echo "<pre>";
         // print_r($result);
@@ -54,24 +54,52 @@
 
         // echo $result[0][0];
 
-        echo "<table border=2 cellspacing=3px>
-        <h1>Students Data Result</h1>   
-        <tr>
-            <th>Name</th>
-            <th>Percentage</th>
-        </tr>";
+        // echo "<table border=2 cellspacing=3px>
+        // <h1>Students Data Result</h1>   
+        // <tr>
+        //     <th>Name</th>
+        //     <th>Percentage</th>
+        //     <th>Percentage</th>
+        // </tr>";
 
-        foreach($result as $key1 => $value1){
-            echo "<tr>";
-                foreach($value1 as $key2 => $value2){
-                    echo "<td>$value2</td>";
-                }
-            echo "</tr>";
-        }
+        // foreach($result as $key1 => $value1){
+        //     echo "<tr>";
+        //         foreach($value1 as $key2 => $value2){
+        //             echo "<td>$value2</td>";
+        //         }
+        //     echo "</tr>";
+        // }
 
 
-        echo "</table>";
-    ?>
+        // echo "</table>";
+
+        // Multidimenstional associative array
+        
+        $marks = [
+            "Abdullah" => ["HTML" => 88, "CSS" => 78, "JS" => 100],
+            "Basim" => ["HTML" => 78, "CSS" => 99, "JS" => 82],
+            "Ammad" => ["HTML" => 90, "CSS" => 67, "JS" => 99],
+            "Hafeez" => ["HTML" => 99, "CSS" => 70, "JS" => 88],
+            "Hassan" => ["HTML" => 87, "CSS" => 94, "JS" => 81]
+        ];
+
+        // echo $marks["Ammad"]["JS"];
+
+
+        echo "<pre>";
+        print_r($marks);
+        echo "</pre>";
+
+    // StdName | HTML | CSS | JS
+    // Abdullah  88      78   100
+
+
+
+?>
+
+
+
+
 
 </body>
 </html>
