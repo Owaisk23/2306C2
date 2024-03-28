8eaba15b5d6cb13e5592b14c6185f5d3b60ad797
Owais Ahmed Khan <owais1566@gmail.com>
2024-03-28 11:58:31 +0500

    added get and post method

---
+++ PHP/Basics/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Super Global Variable in PHP</h1>
+    <form action="./submit.php" method="post">
+        <input type="text" name="username" id="uname" placeholder="Enter Your Name" required>
+        <input type="text" name="qualification" id="quali" placeholder="Enter Your Qualification" required>
+        <input type="submit" value="Submit Form" name="userinfo">
+    </form>
+</body>
+</html>
