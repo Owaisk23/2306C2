19a00d13b6c8019e231f03eaac598e615ac81ab2
Owais Ahmed Khan <owais1566@gmail.com>
2024-10-12 12:57:26 +0500

    C# First class code

---
+++ C#/basics/basics/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, In C# First Class!!");
+
+//string firstName = "Abdullah";
+//string lastName = "Nasir";
+
+//Console.WriteLine("Hello, Welcome in our company Mr. " + firstName + " " + lastName);
+
+// DATATYPES
+//byte age = 23;
+
+//short salary = 27000;
+
+//int number = 7432;
+
+//long largeNum  = 134614612612461L;
+
+//float marks = 77.21F;
+
+//double dNum = 125.992D;
+
+//decimal deciNumber = 8009.123M;
+
+//// string datatypes
+
+//char grade = 'B';
+
+//string address = "North Nazimabad KHI";
+
+//bool isValid = true;
+
+//Console.WriteLine("Age is: " + age + " Salary is: " + salary + " Number is: " + number +
+//    " Large Number is: " + largeNum + " Marks is: " + marks + " dNumber is: " + dNum +
+//    " DeciNum is: " + deciNumber + " Grade is: " + grade + " Address is: " + address +
+//    " isValid is: " + isValid);
+
+// Arithematic Operators
+
+Console.WriteLine(70 + 20);
+Console.WriteLine(70 - 20);
+Console.WriteLine(70 * 20);
+Console.WriteLine(70 / 20);
+Console.WriteLine(70 % 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
