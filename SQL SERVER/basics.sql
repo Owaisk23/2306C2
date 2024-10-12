94788236147b9ac69cf75feb38d1bb1ac779ba72
Owais Ahmed Khan <owais1566@gmail.com>
2024-10-12 12:58:15 +0500

    Triggers in SQL SERVER

--- SQL SERVER/basics.sql
+++ SQL SERVER/basics.sql
@@ -219,6 +219,195 @@ GRANT SELECT on dbo.Employee TO FACULTY_EMP;
 
 GRANT INSERT, DELETE on dbo.Employee TO FACULTY_EMP;
 
+-- REVOKE(To take back the permission)
 REVOKE DELETE on dbo.Employee TO FACULTY_EMP;
 
 
+
+-- PROCEDURES
+
+CREATE PROCEDURE SeeEmp
+AS
+BEGIN
+SELECT * FROM Employee
+END;
+
+SeeEmp;
+
+
+ALTER PROCEDURE SeeEmp
+AS
+BEGIN
+SELECT * FROM Employee where city = 'KHI'; 
+END;
+
+SeeEmp;
+
+CREATE PROCEDURE AddEmp @Name varchar(255), @desig varchar(70), @sal int, @city varchar(60), @dId int
+AS
+BEGIN
+INSERT INTO Employee VALUES(@Name, @desig, @sal, @city, @dId)
+SELECT * FROM Employee
+END;
+
+AddEmp @Name='Haseeb', @desig='Software Developer', @sal=50000, @city='Karachi', @dId=2;
+
+AddEmp @Name='Ali Zaib', @desig='MERN Developer', @sal=250000, @city='Karachi', @dId=3;
+
+DROP PROCEDURE SeeEmp;
+
+SeeEmp;
+
+DELETE FROM Employee where id = 16;
+
+-- indexes 
+
+create index empname_index
+On Employee(empName,salary);
+
+select * from Employees where empName='Owais';
+
+drop index employee.empname_index;
+
+-- Triggers
+
+ -- for
+ CREATE TRIGGER AddEmp_trigger 
+ On Employee
+ for INSERT
+ as
+ BEGIN
+ print('a new employee added successfully.')
+ END;
+
+ INSERT INTO Employee(empName,designation,salary,city,deptId) VALUES
+('Taha ','Mob developer', 58788, 'swat',1);
+
+
+ -- alter
+ ALTER TRIGGER AddEmp_trigger 
+ On Employee
+ for INSERT
+ as
+ BEGIN
+SELECT * FROM inserted;
+ END;
+
+ --Creating audit table
+ CREATE TABLE EmpLogs(
+ logid int Primary key identity(1,1),
+ ActionPerformed nvarchar(255)
+ );
+ SELECT * FROM EmpLogs;
+
+
+ SELECT * into Test from Employees;
+ SELECT * From Test;
+
+ -- Maintaining logs on tables
+
+  create TRIGGER AddEmp_trigger 
+ On Employees
+ AFTER INSERT
+ as
+ BEGIN
+DECLARE @Id int, @name varchar(50)
+SELECT @Id=id, @name=empName FROM inserted
+INSERT INTO EmpLogs VALUES(@name+' having Id = '+ CAST(@Id as varchar(6)) +' is added at '+ cast(GETDATE() as varchar(30)))
+ END;
+
+
+ -- instead of
+
+ SELECT * from Employee;
+ Alter TABLE Employee ADD emp_status int default(1) not null;
+
+
+ CREATE Trigger Soft_Delete
+ On Employee
+ INSTEAD OF DELETE
+ AS
+ BEGIN
+ DECLARE @ID int
+ SELECT @ID=id from Deleted
+ Update Employee set emp_status=0 where id=@ID
+ END;
+
+ DELETE from Employee where id=15;
+
+
+ -- Update Trigger
+ ALTER Trigger Update_Employees
+ On Employees
+ for Update
+ as
+ begin
+DECLARE @Id int
+DECLARE @Newname varchar(60), @Oldname varchar(60)
+DECLARE @Newsalary int,  @Oldsalary int
+DECLARE @NewDesignation varchar(60),@OldDesignation varchar(60)
+DECLARE @Newdeptid int,  @Olddeptid int
+DECLARE @Newcity varchar(60), @Oldcity varchar(60)
+DECLARE @AuditString varchar(255)
+
+SELECT * into #Temptable from inserted
+WHILE(exists (SELECT id from #Temptable))
+begin
+SELECT @Id=id, @Newname=empName, @Newsalary=salary, @NewDesignation=designation,@Newcity=city,@Newdeptid=deptId FROM #Temptable
+SELECT  @Oldname=empName, @Oldsalary=salary, @OldDesignation=designation,@Oldcity=city,@Olddeptid=deptId FROM deleted where id=@Id
+SET @AuditString =''
+SET @AuditString ='An employee having id = '+ CAST(@Id as varchar(6))+' on '+  CAST(GETDATE() as varchar(30))+' is changed '
+if(@Oldname <> @Newname)
+SET @AuditString = @AuditString + ' its name from '+@Oldname +' to '+ @Newname
+
+if(@Oldsalary <> @Newsalary)
+SET @AuditString = @AuditString + ' its salary from '+CAST(@Oldsalary as varchar(10)) +' to '+CAST(@Newsalary as varchar(10))
+
+if(@OldDesignation <> @NewDesignation)
+SET @AuditString = @AuditString + ' its designation from '+@OldDesignation +' to '+ @NewDesignation
+
+if(@Oldcity <> @Newcity)
+SET @AuditString = @AuditString + ' its city from '+@Oldcity +' to '+ @Newcity
+
+if(@Olddeptid <> @Newdeptid)
+SET @AuditString = @AuditString + ' its deptid from '+CAST(@Olddeptid as varchar(10)) +' to '+CAST(@Newdeptid as varchar(10))
+ Insert into Emplogs values(@AuditString)
+ DELETE FROM #Temptable where id=@Id
+end
+ end;
+
+
+ select * from Employees;
+ update Employees set city='karachi' where id > 4;
+ select * from Emplogs;
+
+ -- Transactions 
+ -- ddl- data definition language (create , alter, drop)
+ -- dml-data manipulation language (insert,update,delete, select)
+ -- dcl-data control languages (grant/revoke)
+ -- tcl- transaction control language. (ROLLBACK/ COMMIT)
+
+ -- ACID properties.
+ 
+-- A => Atomicity -- all the steps are fully completed or not a single step will execute.
+-- C => Consistency -- all the nodes in a network should be aware of the transaction.
+-- I => Isolation -- one transaction's info should not be known to another transaction.
+-- D => Durability -- changes after transaction should be saved.
+
+-- Integrity
+Begin Transaction
+Update Employees set empName='Khurram' where id= 1;
+Commit Transaction;-- save changes
+
+Rollback Transaction;-- wapis
+
+BEGIN TRANSACTION
+BEGIN try 
+Update Employees set empName='chakwal' where id= 1/0;
+print('Commited Successfully.')
+COMMIT TRANSACTION
+End try
+BEGIN catch
+print('Rolled back Successfully.')
+Rollback Transaction
+END catch;
