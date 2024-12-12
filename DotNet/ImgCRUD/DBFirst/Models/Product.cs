e4f75efbe6b08c18e85fa0e46a1a1c3fd2d38285
Owais Ahmed Khan <owais1566@gmail.com>
2024-12-12 11:59:09 +0500

    img crud complete

---
+++ DotNet/ImgCRUD/DBFirst/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirst.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public int Price { get; set; }
+
+    public string Image { get; set; } = null!;
+
+    public int? CatId { get; set; }
+
+    public virtual Category? Cat { get; set; }
+}
---
+++ DotNet/ImgCRUD/DBFirst/Models/ProductsContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirst.Models;
+
+public partial class ProductsContext : DbContext
+{
+    public ProductsContext()
+    {
+    }
+
+    public ProductsContext(DbContextOptions<ProductsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=.;initial catalog=products;user id=sa;password=aptech; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CatId).HasName("PK__category__17B6DD062EF79FBD");
+
+            entity.ToTable("category");
+
+            entity.Property(e => e.CatId).HasColumnName("catId");
+            entity.Property(e => e.CatName)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("catName");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3214EC076F1C9DEC");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.CatId).HasColumnName("catId");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.Image)
+                .HasMaxLength(250)
+                .HasColumnName("image");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength()
+                .HasColumnName("name");
+            entity.Property(e => e.Price).HasColumnName("price");
+
+            entity.HasOne(d => d.Cat).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CatId)
+                .HasConstraintName("FK_products_ToTable");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
