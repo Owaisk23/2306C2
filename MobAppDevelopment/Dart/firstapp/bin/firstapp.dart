148ee691d4168ac638b08bf00e952523ab30ba3e
owaisk23 <owais1566@gmail.com>
2025-03-22 11:56:26 +0500

    added dart variables types

---
+++ MobAppDevelopment/Dart/firstapp/bin/firstapp.dart
@@ -0,0 +1,5 @@
+import 'package:firstapp/firstapp.dart' as firstapp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${firstapp.calculate()}!');
+}
---
+++ MobAppDevelopment/Dart/firstapp/lib/firstapp.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+void main() {
+  // print("Hello World");
+  print("Welcome to Dart Programming");
+
+  // stdout.write("Enter your firstname: ");
+  // stdout.write("Enter your lastname: ");
+
+  //Variables datatypes
+  var carName = "Toyota";
+  const carYear = 2009;
+  final carModel = "Corolla";
+  dynamic carPrice = 1000000;
+  stdout.write("Car Name: $carName\n");
+  stdout.write("Car Year: $carYear\n");
+  stdout.write("Car Model: $carModel\n");
+  stdout.write("Car Price: $carPrice\n");
+  //Numeric values
+  //both positive and negative values
+  int age = 20;
+  //floating values ya point values
+  double height = 5.6;
+  //contain both point and whole numbs
+  num salary = 100;
+
+  //String values
+  String name = "Owais Ahmed Khan";
+
+  print("My name is $name");
+  print("My age is $age");
+  print("My height is $height");
+  print("My salary is $salary");
+
+  //Boolean values
+  bool isAlive = true;
+  print("Am I alive? $isAlive");
+
+  //Conditional Statements
+  // if else
+  stdout.write("Enter your age: ");
+  int driverAge = 10;
+
+
+  if (driverAge >= 18) {
+    print("You are allowed to drive");
+  } else {
+    print("You are not allowed to drive");
+  }
+
+
+
+
+
+
+
+
+
+
+}
