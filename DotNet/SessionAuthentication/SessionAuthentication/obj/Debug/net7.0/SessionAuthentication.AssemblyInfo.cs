a561bbe2fcd179c05faecfcce6f144c3c6e49917
owaisk23 <owais1566@gmail.com>
2025-07-24 12:02:20 +0500

    added insert many

--- DotNet/SessionAuthentication/SessionAuthentication/obj/Debug/net7.0/SessionAuthentication.AssemblyInfo.cs
+++ DotNet/SessionAuthentication/SessionAuthentication/obj/Debug/net7.0/SessionAuthentication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SessionAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2540fd8df32825589ea1335fb1553fc12a550483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d94114974cd82567894af615fea0831fc7a843")]
 [assembly: System.Reflection.AssemblyProductAttribute("SessionAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SessionAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
