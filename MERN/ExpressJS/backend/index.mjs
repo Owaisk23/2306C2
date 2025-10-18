e55e8b65f2a7407e8c410bccaebeda3e13ff2886
owaisk23 <owais1566@gmail.com>
2025-10-18 12:55:05 +0500

    added restful apis crud

--- MERN/ExpressJS/backend/index.mjs
+++ MERN/ExpressJS/backend/index.mjs
@@ -27,16 +27,16 @@ app.get('/about', (req, res) => {
   res.send('Hello from About Page!')
 })
 app.get('/image', (req, res) => {
-  
-    res.sendFile(`${dirname}/static/pixel.jpg`);
+
+  res.sendFile(`${dirname}/static/pixel.jpg`);
 })
 
-app.use('/home', express.static(dirname, {index: '/static/index.html'}));
+app.use('/home', express.static(dirname, { index: '/static/index.html' }));
 
 // Router Parameters (The are compulsory)
-app.get('/product/:id', (req, res) => {
-  res.json({name: 'Product No: ' + req.params.id});
-})
+// app.get('/product/:id', (req, res) => {
+//   res.json({ name: 'Product No: ' + req.params.id });
+// })
 
 app.get('/posts/:id', (req, res) => {
   let postId = req.params.id;
@@ -59,22 +59,22 @@ app.get('/posts/:id', (req, res) => {
   ];
 
   postObj.map((post) => {
-    if(post.postId == postId){
+    if (post.postId == postId) {
       console.log(post);
       postObj = post;
     }
   })
-    res.json(postObj);
+  res.json(postObj);
 })
 
 
 // Query Parameters (They are option)
 app.get('/categories', (req, res) => {
-  if(req.query.name){
-    res.json({name: "Category: " + req.query.name})
+  if (req.query.name) {
+    res.json({ name: "Category: " + req.query.name })
   }
   else {
-    res.json({name: "All Categories"})
+    res.json({ name: "All Categories" })
   }
 })
 
@@ -84,21 +84,79 @@ app.get('/contact', (req, res) => {
   // const age = req.body.age;
   // const city = req.body.city;
 
-    // Destructing
-    const {name, age, city} = req.body;
-  res.json({name: name, age: age, city: city})
+  // Destructing
+  const { name, age, city } = req.body;
+  res.json({ name: name, age: age, city: city })
 })
 
+//fetch data
 app.get('/products', (req, res) => {
-  try{
-    res.status(200).json({message: "Showing our products", products: products});
-  }catch(error){
+  try {
+    res.status(200).json({ message: "Showing our products", products: products });
+  } catch (error) {
     console.log(error);
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
   }
 })
 
+//fetch data by id
+app.get('/product/:id', (req, res) => {
+  try {
+    let id = req.params.id
+    let product = products.find((prd) => {
+      return prd.id == id;
+    })
+     if (product) {
+      res.status(200).json({ message: "Product found", products: product });
+    } else {
+      res.status(404).json({ message: "No product found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message })
+  }
+})
 
+//Add Product 
+app.post('/addproduct', (req, res) => {
+  try {
+    let newProd = req.body;
+    let addProduct = products.push(newProd);
+    console.log(newProd);
+    if (addProduct) {
+      res.status(200).json({ message: "Product added", products: newProd });
+    } else {
+      res.status(404).json({ message: "No product found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message })
+  }
+})
+
+// //Delete product
+
+app.delete('/deleteproduct/:id', (req, res) =>{
+  try {
+    let id = req.params.id;
+    let deletedProduct= products.find((prd)=>{
+  return prd.id == id
+})
+let filteredProducts= products.filter((item)=>{item.id == id})
+console.log(deletedProduct);
+products=filteredProducts;
+
+
+if (filteredProducts) {
+  res.status(200).json({message:"Product deleted successfully",product:deletedProduct});
+} else {
+  res.status(404).json({message:"Can't delete right now"});
+}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message:error.message})
+  }
+})
 
 
 
